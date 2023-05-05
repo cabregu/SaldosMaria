090b3def698dc9e69e690195c4432480c1bcd6c4
Cristian Abregu <cristian.g.abregu@gmail.com>
2023-05-05 17:07:29 -0300

    Lectura OCR

--- SaldosMaria/FrmLeerImagen.vb
+++ SaldosMaria/FrmLeerImagen.vb
@@ -7,10 +7,10 @@ Public Class FrmLeerImagen
 
     Private Sub FrmLeerImagen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim bmp As Bitmap = Image.FromFile("C:\Users\Cristian\Desktop\TEMP\4.jpg")
-        PictureBox1.SizeMode = PictureBoxSizeMode.CenterImage
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
         PictureBox1.Image = bmp
+        PictureBox1.Size = bmp.Size
     End Sub
-
     Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
         startPoint = e.Location
         isDragging = True
@@ -26,30 +26,39 @@ Public Class FrmLeerImagen
     Private Sub PictureBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseUp
         isDragging = False
         Dim rect As New Rectangle(
-        Math.Min(startPoint.X, endPoint.X),
-        Math.Min(startPoint.Y, endPoint.Y),
-        Math.Abs(startPoint.X - endPoint.X),
-        Math.Abs(startPoint.Y - endPoint.Y))
-        ' Eliminar la lectura OCR del bitmap
-        ' Dim ocr = New AutoOcr()
+            Math.Min(startPoint.X, endPoint.X),
+            Math.Min(startPoint.Y, endPoint.Y),
+            Math.Abs(startPoint.X - endPoint.X),
+            Math.Abs(startPoint.Y - endPoint.Y))
         Dim bmp As Bitmap = PictureBox1.Image
         Dim croppedBmp As Bitmap = bmp.Clone(rect, bmp.PixelFormat)
-        ' Eliminar la llamada a la lectura OCR del bitmap
-        ' Dim result = ocr.Read(croppedBmp)
-        ' MsgBox(result.Text)
+        PictureBox1.Image = croppedBmp
+        PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
     End Sub
 
     Private Sub PictureBox1_Paint(sender As Object, e As PaintEventArgs) Handles PictureBox1.Paint
         If isDragging Then
             Dim rect As New Rectangle(
-            Math.Min(startPoint.X, endPoint.X),
-            Math.Min(startPoint.Y, endPoint.Y),
-            Math.Abs(startPoint.X - endPoint.X),
-            Math.Abs(startPoint.Y - endPoint.Y))
+                Math.Min(startPoint.X, endPoint.X),
+                Math.Min(startPoint.Y, endPoint.Y),
+                Math.Abs(startPoint.X - endPoint.X),
+                Math.Abs(startPoint.Y - endPoint.Y))
             e.Graphics.DrawRectangle(Pens.Red, rect)
         End If
     End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Obtener la imagen actual en el PictureBox
+        Dim bmp As Bitmap = PictureBox1.Image
 
+        ' Crear un objeto IronOcr OCR
+        Dim Ocr = New AutoOcr()
 
+        ' Leer el texto de la imagen
+        Dim Resultado = Ocr.Read(bmp)
+
+        ' Mostrar el texto en un cuadro de texto
+        MsgBox(Resultado.ToString)
+
+    End Sub
 
 End Class
